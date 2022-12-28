5926afbbd56d897a8a9ac38fc27559be9aa71c79
BitawareUnleashed <dario.benevento@gmail.com>
2022-12-28 14:34:14 +0100

    First commit

---
+++ SignalR.Server/SignalR.Server/Models/ChartModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Server.Models;
+
+public class ChartModel
+{
+    public List<int> Data { get; set; }
+    public string? Label { get; set; }
+    public string? BackgroundColor { get; set; }
+
+    public ChartModel()
+    {
+        Data = new List<int>();
+    }
+}
+
+public class ChartHub : Hub
+{
+}
